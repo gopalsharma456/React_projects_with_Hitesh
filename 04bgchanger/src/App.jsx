42dbef087c0a29e5467608207f1e5893fab19b92
gopal <gopalsharma456@gmail.com>
2023-10-07 13:19:50 +0530

    projects add

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "red" }}
+            onClick={() => {
+              setColor("red");
+            }}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "Green" }}
+            onClick={() => {
+              setColor("green");
+            }}
+          >
+            Green
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "Blue" }}
+            onClick={() => {
+              setColor("blue");
+            }}
+          >
+            Blue
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "Yellow" }}
+            onClick={() => {
+              setColor("yellow");
+            }}
+          >
+            Yellow
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "Purple" }}
+            onClick={() => {
+              setColor("purple");
+            }}
+          >
+            Purple
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
