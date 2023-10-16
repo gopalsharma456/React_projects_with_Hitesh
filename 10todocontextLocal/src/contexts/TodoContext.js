3400730144a916e4ead9d68c84626b0a8af96d2a
gopal <gopalsharma456@gmail.com>
2023-10-16 12:38:27 +0530

    bug fix

--- 10todocontextLocal/src/contexts/TodoContext.js
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -1,21 +1,22 @@
-import { useContext, createContext } from "react";
+import {createContext, useContext} from "react"
 
-const TodoContext = createContext({
-  todos: [
-    {
-      id: 1,
-      todo: " Todo msg",
-      completed: false,
-    },
-  ],
-  addTodo: (todo) => {},
-  updateTodo: (id, todo) => {},
-  deleteTodo: (id) => {},
-  toggleComplete: (id) => {},
-});
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
 
 export const useTodo = () => {
-  return useContext(TodoContext);
-};
+    return useContext(TodoContext)
+}
 
-export const TodoProvider = TodoContext.Provider;
+export const TodoProvider = TodoContext.Provider
