29b19269caf4496df4b72f14f696427664beb1a3
gopal <gopalsharma456@gmail.com>
2023-10-17 15:41:37 +0530

    Redux Todo

---
+++ 11reduxTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, text: "Hello" }],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      state.todos = state.todos.map((prev) =>
+        prev.id === action.payload ? action.payload : prev
+      );
+    },
+  },
+});
+
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
